8b96c24a72e3d6dc941eee75230d7690d7dc041d
Soulayy <s.sultaway@gmail.com>
2024-02-19 16:44:58 +0100

    creat nav and comp article

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import './App.css';
-import Firstcomp from './Compoments/Firstcomp/Firstcomp';
+import React, { useState } from 'react'
+import Firstcomp from './Compoments/Firstcomp/Firstcomp'
+import Data from "./json/Data.json"
 
-function App() {
+import Zelda from "./img/zelda.avif"
+import Logo from "./img/logo.jpg"
+import Avatar from './img/avatar.jpg'
+
+
+
+export default function App() {
+
+
+
+  const [monny, setMonny] = useState(0)
+  const [article, setArticle] = useState(0)
+
+  let ajout = () => {
+    setArticle(article+1)
+  }
+
+
+
   return (
-    <div className="App h-screen w-screen bg-black flex justify-center items-center">
-      <h1 className='text-white text-9xl font-bold hover:text-red-700'>TRIBELHA ON TOP</h1>
-      <Firstcomp></Firstcomp>
+    <div className='bg-blue-500 h-full w-fit flex items-center flex-col'>
+                <div className="navbar bg-white opacity-60">
+            <div className="flex-1">
+              <a className="btn btn-ghost"><img onClick={() => ajout()} className='h-[100%] w-[100%]' src={Logo} alt="" /> </a>
+            </div>
+            <div className="flex-none">
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                  <div className="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    <span className="badge badge-sm indicator-item">{article}</span>
+                  </div>
+                </div>
+                <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                  <div className="card-body">
+                    <span className="font-bold text-lg">{article} Items</span>
+                    <span className="text-info">Subtotal: ${monny}</span>
+                    <div className="card-actions">
+                      <button className="btn btn-primary btn-block">View cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img alt="Tailwind CSS Navbar component" src={Avatar} />
+                  </div>
+                </div>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <a className="justify-between">
+                      Profile
+                      <span className="badge">New</span>
+                    </a>
+                  </li>
+                  <li><a>Settings</a></li>
+                  <li><a>Logout</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+    <div className=' w-[80%] bg-red-500 flex justify-center items-center'>
+      <Firstcomp Data={Data} Zelda={Zelda}></Firstcomp>
     </div>
-  );
+    </div>
+  )
 }
-
-export default App;
--- src/Compoments/Firstcomp/Firstcomp.jsx
+++ src/Compoments/Firstcomp/Firstcomp.jsx
@@ -1,9 +1,31 @@
 import "./Firstcomp.css";
 
-export default function Firstcomp() {
+
+export default function Firstcomp(props) {
+    
     return (
-        <div>
-        
+        <div className="h-[90%] w-[90%] flex justify-center items-center flex-wrap gap-10">
+            
+            {
+                props.Data.map((element) => {
+                    return(
+                    <div className="card w-[350px] h-[550px] bg-base-100 shadow-xl">
+                        <figure className="px-10 pt-10">
+                            <img  src={props.Zelda} alt="Shoes" className="rounded-xl h-[100%] w-[70%]" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{element.nom}</h2>
+                            <p>Genre : {element.genre}</p>
+                            <p>Quantité : {element.qualite}</p>
+                            <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    )
+                })
+            }
+            )
         </div>
     )
 }
